3438420e53a5a60dfccd17ddecc1737eb18c211d
Silvan Adrian <hallo@silvanadrian.ch>
2018-12-07 13:21:44 +0100

    use a data faker library for creating random books, wip

--- assignments/assignment3/acertainbookstore-assignment3/src/com/acertainbookstore/client/tests/BookSetGeneratorTest.java
+++ assignments/assignment3/acertainbookstore-assignment3/src/com/acertainbookstore/client/tests/BookSetGeneratorTest.java
@@ -1,5 +1,7 @@
 package com.acertainbookstore.client.tests;
 
+import com.acertainbookstore.business.Book;
+import com.acertainbookstore.business.StockBook;
 import com.acertainbookstore.client.workloads.BookSetGenerator;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,5 +25,11 @@ public class BookSetGeneratorTest {
     public void shouldReturnRandomISBNS() {
         Set<Integer> isbns = IntStream.rangeClosed(1, 9000).boxed().collect(Collectors.toSet());
         assertEquals(30,bookSetGenerator.sampleFromSetOfISBNs(isbns,30).size());
+    }
+
+    @Test
+    public void shouldReturnRandomBooks() {
+        Set<StockBook> bookSet = bookSetGenerator.nextSetOfStockBooks(100);
+        assertEquals(100, bookSet.size());
     }
 }
--- assignments/assignment3/acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ assignments/assignment3/acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -1,19 +1,26 @@
 package com.acertainbookstore.client.workloads;
 
+import com.acertainbookstore.business.Book;
+import com.acertainbookstore.business.ImmutableStockBook;
 import com.acertainbookstore.business.StockBook;
+import com.github.javafaker.Faker;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Helper class to generate stockbooks and isbns modelled similar to Random
  * class
  */
 public class BookSetGenerator {
+
+    private static Integer ISBN = 1;
 
 	public BookSetGenerator() {
 		// TODO Auto-generated constructor stub
@@ -38,7 +45,27 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<StockBook> nextSetOfStockBooks(int num) {
-		return null;
+	    Set<StockBook> result = new HashSet<>();
+        IntStream.rangeClosed(1, num).forEach(i -> result.add(createRandomBook()));
+		return result;
 	}
+
+    private ImmutableStockBook createRandomBook() {
+        Faker faker = new Faker();
+	    final String title = faker.book().title();
+	    final String author = faker.book().author();
+        float price = 5.0f;
+        int copies = 5;
+        long misses = 1L;
+        long rated = 1L;
+        long rating = 1L;
+        boolean editorPick = false;
+
+        return new ImmutableStockBook(getISBN(),title,author, price, copies,misses,rated,rating,editorPick);
+    }
+
+    private Integer getISBN() {
+	    return ISBN++;
+    }
 
 }
